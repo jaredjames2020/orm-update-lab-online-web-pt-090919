9badd906bf2db426867fc22704ac7f9a288c2902
Jared S <jaredjames2020@gmail.com>
2019-11-16 23:46:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,9 +58,9 @@ class Student
   
   def self.new_from_db(new_student_from_db)
     new_student = self.new(ew_student_from_db[0], new_student_from_db[1], new_student_from_db[2])
-    new_student.id = new_student_from_db[0]
-    new_student.name =  new_student_from_db[1]
-    new_student.grade = new_student_from_db[2]
+    # new_student.id = new_student_from_db[0]
+    # new_student.name =  new_student_from_db[1]
+    # new_student.grade = new_student_from_db[2]
     new_student
   end
     
