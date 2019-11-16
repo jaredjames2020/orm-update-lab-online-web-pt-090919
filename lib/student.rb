70c1e2d3c0adae48cfd0a70ca0145f99bac728d8
Jared S <jaredjames2020@gmail.com>
2019-11-16 23:33:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,8 @@ class Student
   end  
   
   def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.name)
   end
     
     
